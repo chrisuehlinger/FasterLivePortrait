9d5879375da83ad0ff0508eff6cbb26185fdf1e0
Chris Uehlinger <chris.uehlinger@gmail.com>
2025-05-13 18:05:05 -0400

    Dial up expressiveness for Chac Bolay

--- src/pipelines/faster_live_portrait_pipeline.py
+++ src/pipelines/faster_live_portrait_pipeline.py
@@ -422,10 +422,10 @@ class FasterLivePortraitPipeline:
                     x_d_exp_smooth = self.exp_smooth.process(x_d_exp_smooth)
                 if self.cfg.infer_params.animation_region in ["all", "exp"]:
                     for idx in [1, 2,  11, 13, 15, 16, 18]:
-                        delta_new[:, idx, :] = x_d_exp_smooth[:, idx, :] if self.is_source_video else 2*x_d_i_info['exp'][
+                        delta_new[:, idx, :] = x_d_exp_smooth[:, idx, :] if self.is_source_video else (5 if self.is_animal else 2)*x_d_i_info['exp'][
                                                                                                       :, idx, :]
                     for idx in [6, 12, 14, 17, 19, 20]:
-                        delta_new[:, idx, :] = x_d_exp_smooth[:, idx, :] if self.is_source_video else 3*x_d_i_info['exp'][
+                        delta_new[:, idx, :] = x_d_exp_smooth[:, idx, :] if self.is_source_video else (10 if self.is_animal else 3)*x_d_i_info['exp'][
                                                                                                       :, idx, :]
                     delta_new[:, 3:5, 1] = x_d_exp_smooth[:, 3:5, 1] if self.is_source_video else x_d_i_info['exp'][:,
                                                                                                   3:5, 1]
